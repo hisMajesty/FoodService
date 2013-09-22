55fd7726a58ef184217f5ee7a487f1ede32542e9
Lutsiv Andriy <lutsiv.andriy@gmail.com>
2013-09-22 23:48:14 +0300

    Header; redirecting unauthorised users from HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
+  before_action :check_if_authorised
+
+  def check_if_authorised
+      unless current_user
+        flash[:warning] = 'not authorized!'
+        redirect_to root_path
+      end
+  end
+
   def index
 
   end
+
+
 end
