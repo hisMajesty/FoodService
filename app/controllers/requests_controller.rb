2b199ce4ed543ffa79ce7fdf0f113c08c6a8607c
nickodyn <nickodyn@gmail.com>
2013-10-16 01:36:18 +0300

    Added missed files #5 #10

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,30 @@
 class RequestsController < ApplicationController
 
+	skip_before_filter :verify_authenticity_token  
+
 	def index
 		@requests = Request.all
 		render 'list'
 	end
 
-	def new
-		request = Request.new
-		request.user_id = current_user().id		
-		request.date = params[:date].to_date
-		request.save
+	def create
+		@request = Request.new
+		puts current_user()
+		@request.user_id = current_user().id		
+		@request.date = params[:date].to_date
+		
 
-		redirect_to '/requests'
+		respond_to do |format|
+			if @request.save	
+			  format.js { render :json => "message" }
+        	  format.html { redirect_to @request, notice: 'User was successfully created.' }
+		      
+		      format.json { render json: @request, status: :created, location: @request }
+		    else
+		      format.html { render action: "new" }
+		      format.json { render json: @request.errors, status: :unprocessable_entity }
+		    end
+		end
 	end
+
 end
